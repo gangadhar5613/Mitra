349c1d3e00376de0f5350c1292133a71d749d83b
Alisha Saxena <alishasaxena2016@gmail.com>
2021-03-05 16:32:29 +0530

    Footer added to develop

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 class App extends React.Component {
   constructor(props) {
@@ -22,40 +22,10 @@ class App extends React.Component {
     return (
       <div>
         <Header />
+        <Footer />
       </div>
     );
   }
 }
 
 export default App;
-=======
-import React from "react";
-import Header from "./Header";
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: null,
-    };
-  }
-
-  async componentDidMount() {
-    const response = await fetch("/api");
-    const { title } = await response.json();
-    this.setState({
-      title,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-      </div>
-    );
-  }
-}
-
-export default App;
->>>>>>> 9b178a01dd66ee7a2051a9dfd71f1c12216ce76f
