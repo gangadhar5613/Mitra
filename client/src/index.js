014c57ddecb6af10a45abc59bb770b5ca8ff1cd0
Alisha Saxena <alishasaxena2016@gmail.com>
2021-03-08 22:19:43 +0530

    header-updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App"
-import "./stylesheet/tailwind.css";
-import "./stylesheet/main.scss";
-
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App"
+import App from "./components/App";
 import "./stylesheet/tailwind.scss";
 import "./stylesheet/main.scss";
 
->>>>>>> 9b178a01dd66ee7a2051a9dfd71f1c12216ce76f
 ReactDOM.render(<App />, document.querySelector("#root"));
