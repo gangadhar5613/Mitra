8d7d177d524ece74805676d42bf7fc90114d2678
Alisha Saxena <alishasaxena2016@gmail.com>
2021-03-18 13:51:57 +0530

    faq update

--- client/src/components/FAQs.jsx
+++ client/src/components/FAQs.jsx
@@ -2,16 +2,20 @@ import React from "react";
 
 const data = [
   {
+    id: 1,
     question: "What is Mitra ?",
     answer:
       "Mitra is an online platform to raise blood request locally and those who are interested to donate they can send request and can send locally.Many can find blood after a couple of hours ,but our platform will make donors available within a mean time,so that no life will loose.",
   },
   {
+    id: 2,
+
     question: "How can I raise the blood request ?",
     answer:
       "For raising blood request you need to register in our platform with all required information,after successful verification,you will get a dashboard with request option.",
   },
   {
+    id: 3,
     question: "How can I donate blood?",
     answer:
       "For donating blood you need to register with our platform with all required information.And user have to submit medical/blood report ,after successful verification,you can check all blood request near by your location in the feed section and can click on donate button.",
@@ -22,39 +26,42 @@ class FAQs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isAnsVisible: false,
+      isAnsVisible: 1,
     };
   }
-  toggleVisible = () => {
-    this.setState((prevState) =>
-    {
+  toggleVisible = (id) => {
+    this.setState((prevState) => {
       return {
-        isAnsVisible:!prevState.isAnsVisible
-      }
-    })
+        isAnsVisible: id,
+      };
+    });
   };
   render() {
     return (
       <div className="pt-8">
-        <h3 className='text-center text-4xl  text-yellow-700 pt-20'>FAQs</h3>
+        <h3 className="text-center text-4xl  text-yellow-700 pt-20">FAQs</h3>
         <div className="App">
           {data.map((item) => (
             <div className="wrap">
               <div
+                id={item.id}
                 className="ques flex justify-between items-center"
-                onClick={this.toggleVisible}
+                onClick={() => this.toggleVisible(item.id)}
               >
                 <p>{item.question}</p>
                 <i
                   className={
-                    this.state.isAnsVisible === true
-                      ? "far cursor-pointer fa-hand-point-up"
-                      : "far cursor-pointer fa-hand-point-down"
+                    this.state.isAnsVisible === item.id
+                      ? "far cursor-pointer fa-hand-point-down"
+                      : "far cursor-pointer fa-hand-point-up"
                   }
                 ></i>
               </div>
               <div
-                className={`ans ${this.state.isAnsVisible ? "visible" : ""}`}
+                id={item.id}
+                className={`ans ${
+                  this.state.isAnsVisible === item.id ? "visible" : ""
+                }`}
               >
                 {item.answer}
               </div>
